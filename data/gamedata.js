18a50dc406f878dafc0f475c9e596b3eb80e76c1
uttamchandra1 <uttamchandra2024@gamil.com>
2024-05-27 15:20:15 +0530

    Added assets

--- data/gamedata.js
+++ data/gamedata.js
@@ -42,49 +42,129 @@ const games = [
       thumbnail:
         "https://play-lh.googleusercontent.com/OTaxlJWXLVvMyblnwe-qtbRhFv_JB_nnl6uZj6AH4j7brQp1X1cJ69QM1hQM1rbATXE=w240-h480-rw",
   
-      assets: [
-        {
-          title: "Meme Template 1",
-          thumbnail:
-            "https://g-mob.glance-cdn.com/public/content/assets/xiaomi/IMG-20240404-WA0043.jpg",
-        },
-        {
-          title: "Meme Template 2",
-          thumbnail:
-            "https://g-mob.glance-cdn.com/public/content/assets/xiaomi/IMG-20240404-WA0042.jpg",
-        },
-  
-        {
-          title: "Meme Template 2",
-          thumbnail:
-            "https://g-mob.glance-cdn.com/public/content/assets/xiaomi/IMG-20240404-WA0041.jpg",
-        },
-        {
-          title: "Meme Template 2",
-          thumbnail:
-            "https://g-mob.glance-cdn.com/public/content/assets/xiaomi/IMG-20240404-WA0040.jpg",
-        },
-        {
-          title: "Meme Template 2",
-          thumbnail:
-            "https://g-mob.glance-cdn.com/public/content/assets/xiaomi/IMG-20240404-WA0039.jpg",
-        },
-        {
-          title: "Meme Template 2",
-          thumbnail:
-            "https://g-mob.glance-cdn.com/public/content/assets/xiaomi/IMG-20240404-WA0038.jpg",
-        },
-        {
-          title: "Meme Template 2",
-          thumbnail:
-            "https://g-mob.glance-cdn.com/public/content/assets/xiaomi/IMG-20240404-WA0037.jpg",
-        },
-        {
-          title: "Meme Template 2",
-          thumbnail:
-            "https://g-mob.glance-cdn.com/public/content/assets/xiaomi/IMG-20240404-WA0036.jpg",
-        },
-      ],
+        assets: [
+          {
+            title: "Meme Template 1",
+            thumbnail:
+              "https://g-mob.glance-cdn.com/public/content/assets/xiaomi/IMG-20240404-WA0043.jpg",
+          },
+          {
+            title: "Meme Template 1",
+            thumbnail:
+              "https://g-mob.glance-cdn.com/public/content/assets/xiaomi/IMG-20240404-WA0044.jpg",
+          },
+          {
+            title: "Meme Template 1",
+            thumbnail:
+              "https://g-mob.glance-cdn.com/public/content/assets/xiaomi/IMG-20240404-WA0045.jpg",
+          },
+          {
+            title: "Meme Template 1",
+            thumbnail:
+              "https://g-mob.glance-cdn.com/public/content/assets/xiaomi/IMG-20240404-WA0046.jpg",
+          },
+          {
+            title: "Meme Template 2",
+            thumbnail:
+              "https://g-mob.glance-cdn.com/public/content/assets/xiaomi/IMG-20240404-WA0042.jpg",
+          },
+    
+          {
+            title: "Meme Template 2",
+            thumbnail:
+              "https://g-mob.glance-cdn.com/public/content/assets/xiaomi/IMG-20240404-WA0041.jpg",
+          },
+          {
+            title: "Meme Template 2",
+            thumbnail:
+              "https://g-mob.glance-cdn.com/public/content/assets/xiaomi/IMG-20240404-WA0040.jpg",
+          },
+          {
+            title: "Meme Template 2",
+            thumbnail:
+              "https://g-mob.glance-cdn.com/public/content/assets/xiaomi/IMG-20240404-WA0039.jpg",
+          },
+          {
+            title: "Meme Template 2",
+            thumbnail:
+              "https://g-mob.glance-cdn.com/public/content/assets/xiaomi/IMG-20240404-WA0038.jpg",
+          },
+          {
+            title: "Meme Template 2",
+            thumbnail:
+              "https://g-mob.glance-cdn.com/public/content/assets/xiaomi/IMG-20240404-WA0037.jpg",
+          },
+          {
+            title: "Meme Template 2",
+            thumbnail:
+              "https://g-mob.glance-cdn.com/public/content/assets/xiaomi/IMG-20240404-WA0036.jpg",
+          },
+          {
+            title: "Meme Template 2",
+            thumbnail:
+              "https://g-mob.glance-cdn.com/public/content/assets/xiaomi/IMG-20240404-WA0035.jpg",
+          },
+          {
+            title: "Meme Template 2",
+            thumbnail:
+              "https://g-mob.glance-cdn.com/public/content/assets/xiaomi/IMG-20240404-WA0034.jpg",
+          },
+          {
+            title: "Meme Template 2",
+            thumbnail:
+              "https://g-mob.glance-cdn.com/public/content/assets/xiaomi/IMG-20240404-WA0033.jpg",
+          },
+          {
+            title: "Meme Template 2",
+            thumbnail:
+              "https://g-mob.glance-cdn.com/public/content/assets/xiaomi/IMG-20240404-WA0023.jpg",
+          },
+          {
+            title: "Meme Template 2",
+            thumbnail:
+              "https://g-mob.glance-cdn.com/public/content/assets/xiaomi/IMG-20240404-WA0024.jpg",
+          },
+          {
+            title: "Meme Template 2",
+            thumbnail:
+              "https://g-mob.glance-cdn.com/public/content/assets/xiaomi/IMG-20240404-WA0025.jpg",
+          },
+          {
+            title: "Meme Template 2",
+            thumbnail:
+              "https://g-mob.glance-cdn.com/public/content/assets/xiaomi/IMG-20240404-WA0026.jpg",
+          },
+          {
+            title: "Meme Template 2",
+            thumbnail:
+              "https://g-mob.glance-cdn.com/public/content/assets/xiaomi/IMG-20240404-WA0027.jpg",
+          },
+          {
+            title: "Meme Template 2",
+            thumbnail:
+              "https://g-mob.glance-cdn.com/public/content/assets/xiaomi/IMG-20240404-WA0028.jpg",
+          },
+          {
+            title: "Meme Template 2",
+            thumbnail:
+              "https://g-mob.glance-cdn.com/public/content/assets/xiaomi/IMG-20240404-WA0029.jpg",
+          },
+          {
+            title: "Meme Template 2",
+            thumbnail:
+              "https://g-mob.glance-cdn.com/public/content/assets/xiaomi/IMG-20240404-WA0030.jpg",
+          },
+          {
+            title: "Meme Template 2",
+            thumbnail:
+              "https://g-mob.glance-cdn.com/public/content/assets/xiaomi/IMG-20240404-WA0031.jpg",
+          },
+          {
+            title: "Meme Template 2",
+            thumbnail:
+              "https://g-mob.glance-cdn.com/public/content/assets/xiaomi/IMG-20240404-WA0032.jpg",
+          },
+        ],
     },
     {
         name: "Marvel Snap",
@@ -136,6 +216,21 @@ const games = [
               "https://g-mob.glance-cdn.com/public/content/assets/xiaomi/IMG-20240404-WA0043.jpg",
           },
           {
+            title: "Meme Template 1",
+            thumbnail:
+              "https://g-mob.glance-cdn.com/public/content/assets/xiaomi/IMG-20240404-WA0044.jpg",
+          },
+          {
+            title: "Meme Template 1",
+            thumbnail:
+              "https://g-mob.glance-cdn.com/public/content/assets/xiaomi/IMG-20240404-WA0045.jpg",
+          },
+          {
+            title: "Meme Template 1",
+            thumbnail:
+              "https://g-mob.glance-cdn.com/public/content/assets/xiaomi/IMG-20240404-WA0046.jpg",
+          },
+          {
             title: "Meme Template 2",
             thumbnail:
               "https://g-mob.glance-cdn.com/public/content/assets/xiaomi/IMG-20240404-WA0042.jpg",
@@ -170,6 +265,71 @@ const games = [
             title: "Meme Template 2",
             thumbnail:
               "https://g-mob.glance-cdn.com/public/content/assets/xiaomi/IMG-20240404-WA0036.jpg",
+          },
+          {
+            title: "Meme Template 2",
+            thumbnail:
+              "https://g-mob.glance-cdn.com/public/content/assets/xiaomi/IMG-20240404-WA0035.jpg",
+          },
+          {
+            title: "Meme Template 2",
+            thumbnail:
+              "https://g-mob.glance-cdn.com/public/content/assets/xiaomi/IMG-20240404-WA0034.jpg",
+          },
+          {
+            title: "Meme Template 2",
+            thumbnail:
+              "https://g-mob.glance-cdn.com/public/content/assets/xiaomi/IMG-20240404-WA0033.jpg",
+          },
+          {
+            title: "Meme Template 2",
+            thumbnail:
+              "https://g-mob.glance-cdn.com/public/content/assets/xiaomi/IMG-20240404-WA0023.jpg",
+          },
+          {
+            title: "Meme Template 2",
+            thumbnail:
+              "https://g-mob.glance-cdn.com/public/content/assets/xiaomi/IMG-20240404-WA0024.jpg",
+          },
+          {
+            title: "Meme Template 2",
+            thumbnail:
+              "https://g-mob.glance-cdn.com/public/content/assets/xiaomi/IMG-20240404-WA0025.jpg",
+          },
+          {
+            title: "Meme Template 2",
+            thumbnail:
+              "https://g-mob.glance-cdn.com/public/content/assets/xiaomi/IMG-20240404-WA0026.jpg",
+          },
+          {
+            title: "Meme Template 2",
+            thumbnail:
+              "https://g-mob.glance-cdn.com/public/content/assets/xiaomi/IMG-20240404-WA0027.jpg",
+          },
+          {
+            title: "Meme Template 2",
+            thumbnail:
+              "https://g-mob.glance-cdn.com/public/content/assets/xiaomi/IMG-20240404-WA0028.jpg",
+          },
+          {
+            title: "Meme Template 2",
+            thumbnail:
+              "https://g-mob.glance-cdn.com/public/content/assets/xiaomi/IMG-20240404-WA0029.jpg",
+          },
+          {
+            title: "Meme Template 2",
+            thumbnail:
+              "https://g-mob.glance-cdn.com/public/content/assets/xiaomi/IMG-20240404-WA0030.jpg",
+          },
+          {
+            title: "Meme Template 2",
+            thumbnail:
+              "https://g-mob.glance-cdn.com/public/content/assets/xiaomi/IMG-20240404-WA0031.jpg",
+          },
+          {
+            title: "Meme Template 2",
+            thumbnail:
+              "https://g-mob.glance-cdn.com/public/content/assets/xiaomi/IMG-20240404-WA0032.jpg",
           },
         ],
       },
@@ -216,49 +376,129 @@ const games = [
         thumbnail:
           "https://play-lh.googleusercontent.com/OTaxlJWXLVvMyblnwe-qtbRhFv_JB_nnl6uZj6AH4j7brQp1X1cJ69QM1hQM1rbATXE=w240-h480-rw",
     
-        assets: [
-          {
-            title: "Meme Template 1",
-            thumbnail:
-              "https://g-mob.glance-cdn.com/public/content/assets/xiaomi/IMG-20240404-WA0043.jpg",
-          },
-          {
-            title: "Meme Template 2",
-            thumbnail:
-              "https://g-mob.glance-cdn.com/public/content/assets/xiaomi/IMG-20240404-WA0042.jpg",
-          },
-    
-          {
-            title: "Meme Template 2",
-            thumbnail:
-              "https://g-mob.glance-cdn.com/public/content/assets/xiaomi/IMG-20240404-WA0041.jpg",
-          },
-          {
-            title: "Meme Template 2",
-            thumbnail:
-              "https://g-mob.glance-cdn.com/public/content/assets/xiaomi/IMG-20240404-WA0040.jpg",
-          },
-          {
-            title: "Meme Template 2",
-            thumbnail:
-              "https://g-mob.glance-cdn.com/public/content/assets/xiaomi/IMG-20240404-WA0039.jpg",
-          },
-          {
-            title: "Meme Template 2",
-            thumbnail:
-              "https://g-mob.glance-cdn.com/public/content/assets/xiaomi/IMG-20240404-WA0038.jpg",
-          },
-          {
-            title: "Meme Template 2",
-            thumbnail:
-              "https://g-mob.glance-cdn.com/public/content/assets/xiaomi/IMG-20240404-WA0037.jpg",
-          },
-          {
-            title: "Meme Template 2",
-            thumbnail:
-              "https://g-mob.glance-cdn.com/public/content/assets/xiaomi/IMG-20240404-WA0036.jpg",
-          },
-        ],
+          assets: [
+            {
+              title: "Meme Template 1",
+              thumbnail:
+                "https://g-mob.glance-cdn.com/public/content/assets/xiaomi/IMG-20240404-WA0043.jpg",
+            },
+            {
+              title: "Meme Template 1",
+              thumbnail:
+                "https://g-mob.glance-cdn.com/public/content/assets/xiaomi/IMG-20240404-WA0044.jpg",
+            },
+            {
+              title: "Meme Template 1",
+              thumbnail:
+                "https://g-mob.glance-cdn.com/public/content/assets/xiaomi/IMG-20240404-WA0045.jpg",
+            },
+            {
+              title: "Meme Template 1",
+              thumbnail:
+                "https://g-mob.glance-cdn.com/public/content/assets/xiaomi/IMG-20240404-WA0046.jpg",
+            },
+            {
+              title: "Meme Template 2",
+              thumbnail:
+                "https://g-mob.glance-cdn.com/public/content/assets/xiaomi/IMG-20240404-WA0042.jpg",
+            },
+      
+            {
+              title: "Meme Template 2",
+              thumbnail:
+                "https://g-mob.glance-cdn.com/public/content/assets/xiaomi/IMG-20240404-WA0041.jpg",
+            },
+            {
+              title: "Meme Template 2",
+              thumbnail:
+                "https://g-mob.glance-cdn.com/public/content/assets/xiaomi/IMG-20240404-WA0040.jpg",
+            },
+            {
+              title: "Meme Template 2",
+              thumbnail:
+                "https://g-mob.glance-cdn.com/public/content/assets/xiaomi/IMG-20240404-WA0039.jpg",
+            },
+            {
+              title: "Meme Template 2",
+              thumbnail:
+                "https://g-mob.glance-cdn.com/public/content/assets/xiaomi/IMG-20240404-WA0038.jpg",
+            },
+            {
+              title: "Meme Template 2",
+              thumbnail:
+                "https://g-mob.glance-cdn.com/public/content/assets/xiaomi/IMG-20240404-WA0037.jpg",
+            },
+            {
+              title: "Meme Template 2",
+              thumbnail:
+                "https://g-mob.glance-cdn.com/public/content/assets/xiaomi/IMG-20240404-WA0036.jpg",
+            },
+            {
+              title: "Meme Template 2",
+              thumbnail:
+                "https://g-mob.glance-cdn.com/public/content/assets/xiaomi/IMG-20240404-WA0035.jpg",
+            },
+            {
+              title: "Meme Template 2",
+              thumbnail:
+                "https://g-mob.glance-cdn.com/public/content/assets/xiaomi/IMG-20240404-WA0034.jpg",
+            },
+            {
+              title: "Meme Template 2",
+              thumbnail:
+                "https://g-mob.glance-cdn.com/public/content/assets/xiaomi/IMG-20240404-WA0033.jpg",
+            },
+            {
+              title: "Meme Template 2",
+              thumbnail:
+                "https://g-mob.glance-cdn.com/public/content/assets/xiaomi/IMG-20240404-WA0023.jpg",
+            },
+            {
+              title: "Meme Template 2",
+              thumbnail:
+                "https://g-mob.glance-cdn.com/public/content/assets/xiaomi/IMG-20240404-WA0024.jpg",
+            },
+            {
+              title: "Meme Template 2",
+              thumbnail:
+                "https://g-mob.glance-cdn.com/public/content/assets/xiaomi/IMG-20240404-WA0025.jpg",
+            },
+            {
+              title: "Meme Template 2",
+              thumbnail:
+                "https://g-mob.glance-cdn.com/public/content/assets/xiaomi/IMG-20240404-WA0026.jpg",
+            },
+            {
+              title: "Meme Template 2",
+              thumbnail:
+                "https://g-mob.glance-cdn.com/public/content/assets/xiaomi/IMG-20240404-WA0027.jpg",
+            },
+            {
+              title: "Meme Template 2",
+              thumbnail:
+                "https://g-mob.glance-cdn.com/public/content/assets/xiaomi/IMG-20240404-WA0028.jpg",
+            },
+            {
+              title: "Meme Template 2",
+              thumbnail:
+                "https://g-mob.glance-cdn.com/public/content/assets/xiaomi/IMG-20240404-WA0029.jpg",
+            },
+            {
+              title: "Meme Template 2",
+              thumbnail:
+                "https://g-mob.glance-cdn.com/public/content/assets/xiaomi/IMG-20240404-WA0030.jpg",
+            },
+            {
+              title: "Meme Template 2",
+              thumbnail:
+                "https://g-mob.glance-cdn.com/public/content/assets/xiaomi/IMG-20240404-WA0031.jpg",
+            },
+            {
+              title: "Meme Template 2",
+              thumbnail:
+                "https://g-mob.glance-cdn.com/public/content/assets/xiaomi/IMG-20240404-WA0032.jpg",
+            },
+          ],
       },
       {
         name: "Marvel Snap",

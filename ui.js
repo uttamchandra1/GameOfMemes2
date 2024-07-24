f0bae2793515b7d204ee59ae555dff5dd97ff1e9
uttamchandra1 <uttamchandra2024@gamil.com>
2024-07-24 09:31:10 +0530

    added start screenn

--- ui.js
+++ ui.js
@@ -2,19 +2,14 @@ const OVERLAY = document.createElement("div");
 OVERLAY.className = "overlay-class";
 
 function loadUi() {
-  // First, display the start screen
   changeScreen(Start_Screen());
-  
-  // Append the overlay to the body
-  document.body.appendChild(OVERLAY);
 
+  document.body.appendChild(OVERLAY);
 
   // After 5 seconds, switch to the home screen
   setTimeout(() => {
     changeScreen(Home_Screen());
   }, 5000);
-  
 }
 
-
-loadUi()
+loadUi();
